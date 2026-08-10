da4d3e0097d4e96cf1f8eafc2b10490b3d4a5447
Евгений Суслов <evgenijsuslov08683@gmail.com>
2026-08-10 21:06:09 +0500

    add script.js for burger menu

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const burger = document.getElementById('burger');
+    const menu = document.getElementById('menu');
+    const nav = document.getElementById('nav');
+
+
+    burger.addEventListener('click', function () {
+        menu.classList.toggle('open');
+        burger.classList.toggle('checked')
+        nav.addEventListener('click', function () {
+            // Сбрасываем overflow
+            nav.style.overflow = 'unset';
+        });
+    });
+
+    // Закрывать меню при клике на ссылку (для удобства)
+    menu.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('open');
+        });
+    });
+});
