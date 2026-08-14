085cc99245ffb2b7cb8f58b870171f646e9d87dd
SuslovQA <evgenijsuslov08683@gmail.com>
2026-08-14 08:50:10 +0500

    delete comments in code

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,26 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const burger = document.getElementById('burger');
-//     const menu = document.getElementById('menu');
-//     const nav = document.getElementById('nav');
-//
-//
-//     burger.addEventListener('click', function () {
-//         menu.style.display='block';
-//         burger.classList.toggle('checked')
-//     //     nav.addEventListener('click', function () {
-//     //         // Сбрасываем overflow
-//     //         nav.style.overflow = 'unset';
-//     //     });
-//     });
-//
-//     // Закрывать меню при клике на ссылку (для удобства)
-//     // menu.querySelectorAll('a').forEach(link => {
-//     //     link.addEventListener('click', () => {
-//     //         menu.classList.remove('open');
-//     //     });
-//     // });
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
 
     const burger = document.getElementById("burger");
